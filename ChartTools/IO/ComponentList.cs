7491b08cc120174481037f510decb670a46862ff
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2024-07-14 15:55:30 -0400

    #70 Model list

---
+++ ChartTools/IO/ComponentList.cs
@@ -0,0 +1,29 @@
+﻿namespace ChartTools.IO;
+
+[Flags]
+public enum DifficultySet : byte
+{
+    None = 0,
+    Easy = 1 << 0,
+    Medium = 1 << 1,
+    Hard = 1 << 2,
+    Expert = 1 << 3,
+    All = Easy | Medium | Hard | Expert
+};
+
+public record ComponentList
+{
+    public bool Metadata { get; set; }
+    public bool SyncTrack { get; set; }
+    public bool GlobalEvents { get; set; }
+
+    public DifficultySet Drums { get; set; }
+    public DifficultySet LeadGuitar { get; set; }
+    public DifficultySet CoopGuitar { get; set; }
+    public DifficultySet RythmGuitar { get; set; }
+    public DifficultySet Bass { get; set; }
+    public DifficultySet GHLGuitar { get; set; }
+    public DifficultySet GHLBass { get; set; }
+    public DifficultySet Keys { get; set; }
+    public DifficultySet Vocals { get; set; }
+}
