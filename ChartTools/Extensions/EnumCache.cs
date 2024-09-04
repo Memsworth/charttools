2a43f3864f25d38f14a3d618371bb52ee1f6a6eb
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2024-09-04 19:33:15 -0400

    Simplify EnumCache init

--- ChartTools/Extensions/EnumCache.cs
+++ ChartTools/Extensions/EnumCache.cs
@@ -2,7 +2,7 @@
 
 internal static class EnumCache<T> where T : struct, Enum
 {
-    public static T[] Values => _values ??= Enum.GetValues<T>().ToArray();
+    public static T[] Values => _values ??= [.. Enum.GetValues<T>()];
     private static T[]? _values;
 
     public static void Clear() => _values = null;
